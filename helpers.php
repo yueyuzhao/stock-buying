6c27ea9db710649a9ecfea8ef067e67b5cf97103
Yueyu <donald@drmer.net>
2021-05-08 06:05:40 +0800

    clean up

--- helpers.php
+++ helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * 调试：打印传递的参数并退出
+ */
 function dd()
 {
 	foreach (func_get_args() as $arg) {
@@ -8,7 +11,13 @@ function dd()
 	die;
 }
 
-function array_add($a1, $a2)
+/**
+ * 两个数组内的值分别相加
+ * @param $a1 array
+ * @param $a2 array
+ * @return array
+ */
+function array_add(array $a1, array $a2): array
 {
 	$res = [];
 	foreach ($a1 as $key => $value) {
